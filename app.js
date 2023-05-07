fa2777aa33e931d94ad96fa7b11074b213eef738
Julian Giraldo Perez <jgpbak@outlook.com>
2023-05-06 19:39:18 -0500

    Changed the port the app is listening to

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 var routes = require('./routes.js')
+const port = 80
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 routes.loadRoutes(app)
-app.listen(3000)
-console.log('Server listening on port 3000')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
