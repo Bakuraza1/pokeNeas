1650cf65077f7ca3ce16a71672124cec36167c5a
Julian Giraldo Perez <jgpbak@outlook.com>
2023-05-06 19:32:50 -0500

    Added os library to get hostname on both controllers, and show it in the views

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -2,14 +2,15 @@ const pockeNeas = require('../pockeNeas')
 let ejs = require('ejs');
 fs = require('fs');    
 util = require('../util')
-let people = ['geddy', 'neil', 'alex'];
+const os = require('os')
 
 
 require('../pockeNeas')
 
 function randomJson(req, res){
     let rand = util.rand(0, 8)
-    let response = { ...pockeNeas.data[rand] }
+    let response = { ...pockeNeas.data[rand]}
+    response['container'] = os.hostname()
     delete response['Imagen']
     delete response['Frase']
     res.send(response)
@@ -17,7 +18,7 @@ function randomJson(req, res){
 
 function randomQuote(req, res){
   let rand = util.rand(0, 8)
-  res.render('quote', {poke: pockeNeas.data[rand]})
+  res.render('quote', {poke: pockeNeas.data[rand], host:  os.hostname()})
 }
 
 module.exports ={
